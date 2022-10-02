91c7883442774c5acc5b36257f0d79dd6cada863
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2022-10-02 23:13:53 +0300

    js exercize 9 task 1 The fibonacci number problem

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 'use strict';
 
-// 2) Напишите функцию, которая принимает в себя 4 числа и возвращает самое большее из них. Если один из аргументов не является числом или их меньше 4 -возвращается 0. Дробные числа разрешены
+// Задача:
+
+// Создайте функцию, которая будет принимать в себя один аргумент-целое положительное число. Она должна возвращать строку, в которой будут через пробел выведены числа Фибоначчи. Причем, их количество должно быть равно переданному аргументу.
+
+// Если переданный аргумент не число - вернуть пустую строку. Решать без применения рекурсии.
 
 // Пример:
 
-// findMaxNumber(1, 5, 6.6, 11); =>  11
+// fib(4) => "0 1 1 2"
 
-// findMaxNumber(1, 5, '6', '10');  =>  0
+// fib(7) => "0 1 1 2 3 5 8"
 
+// fib('7') => ""
 
-function findMaxNumber(a, b, c, d) {
-    if (typeof a !== 'number' || 
-        typeof b !== 'number' || 
-        typeof c !== 'number' || 
-        typeof d !== 'number') {
-        return console.log(0);
+// fib(1) => "0"
+
+// fib(0) => ""
+
+function fib(num) {
+    let result = [];
+    let strResult = "";
+    if (typeof num !== 'number' || num === 0 || !Number.isInteger(num)) {
+        return console.log('""');
     } else {
-        return console.log(Math.max(a, b, c, d));
+        for (let i = 0; i < num; i++) {
+            if (i > 1) {
+               result[i] = result[i-2] + result[i-1]; 
+            } else {
+                result[i] = i;
+            }
+
+            if (i+1 === num) {
+                strResult += result[i];
+                return console.log(strResult);
+            } else {
+                strResult += result[i];
+                strResult += ' ';
+            }
+        }
     }
 }
 
-findMaxNumber('1', 25, 6.6, 11);
+fib(4.1);
